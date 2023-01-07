12d911ee7944584b2cd23d5a9ebac0aa03cf9944
warriorBunny013 <mona23sonai@gmail.com>
2023-01-07 23:32:19 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+        poppins: ["sans-serif"],
+      },
+      colors: {
+        'regal-blue': '#243c5a',
+      }
+    },
+  },
+  plugins: [],
+};
